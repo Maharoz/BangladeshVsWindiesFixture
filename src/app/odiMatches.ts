0dbaf3ac345b295fee3f2dfba5f801db973cd620
Maharoz <mahruj66@gmail.com>
2018-11-17 18:12:45 +0600

    update

--- src/app/odiMatches.ts
+++ src/app/odiMatches.ts
@@ -2,7 +2,7 @@ import { Match } from './match';
 
 export const ODIMATCHES : Match[] = [
 
-{MatchNo : '1st' , Venue: 'Dhaka' , Date: '9/12 (2.00 pm) Sunday'},
-{MatchNo : '2nd' , Venue: 'Dhaka' , Date: '11/12 (2.00 pm) Tuesday'},
-{MatchNo : '3rd' , Venue: 'Sylhet' , Date: '14/12 (2.00 pm)Friday'}
+{MatchNo : '1st' , Venue: 'Dhaka' , Date: '9/12 (1.30 pm) Sunday'},
+{MatchNo : '2nd' , Venue: 'Dhaka' , Date: '11/12 (1.30 pm) Tuesday'},
+{MatchNo : '3rd' , Venue: 'Sylhet' , Date: '14/12 (1.30 pm) Friday'}
 ];
--- src/app/tTwentyMatches.ts
+++ src/app/tTwentyMatches.ts
@@ -2,9 +2,9 @@ import { Match } from './match';
 
 export const TWENTY : Match[] = [
 
-{MatchNo : '1st' , Venue: 'Sylhet' , Date: '17/12 (6.00 pm) Monday'},
-{MatchNo : '2nd' , Venue: 'Dhaka' , Date: '20/12 (6.00 pm) Thursday'},
-{MatchNo : '3rd' , Venue: 'Dhaka' , Date: '22/12 (6.00 pm) Saturday'}
+{MatchNo : '1st' , Venue: 'Sylhet' , Date: '17/12 (5.30 pm) Monday'},
+{MatchNo : '2nd' , Venue: 'Dhaka' , Date: '20/12 (5.30 pm) Thursday'},
+{MatchNo : '3rd' , Venue: 'Dhaka' , Date: '22/12 (5.30 pm) Saturday'}
 
 
 ];
